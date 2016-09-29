06f966b6bdad6f50e20cc4659d0543b62875b26d
Ting Chen Leo Hsu <iteloolab@gmail.com>
2016-09-29 13:57:43 +0900

    Implements printing and exception effects using sum types and GADT - [todo] replace sum types with unions - [todo] actually use the exception effects

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE PartialTypeSignatures #-}
 
 module Main where
 
@@ -15,16 +18,11 @@ import System.IO.Unsafe (unsafePerformIO)
 
 main :: IO ()
 main = do
-  -- spec <- readFile "packet_spec"  -- not closed
-  -- r <- streamFromFile "packet_spec" getlines
   let header = BS.take 5 . BS.drop 42
-  r <- streamPackets "mdf-kospi200.20110216-0.pcap"
-          $ filterPackets ((== BS.pack "B6034") . header)
-          $ take' 8
-          $ getPackets
-  print r
-
-
+  streamPackets "mdf-kospi200.20110216-0.pcap"
+    $ filterPackets ((== BS.pack "B6034") . header)
+    $ take' 20
+    $ getPackets
 
 -- quote ac
 -- Your program should print the packet and quote accept times,
@@ -32,6 +30,7 @@ main = do
 -- followed by the bids from 5th to 1st,
 -- then the asks from 1st to 5th
 type QuotePacket = BS.ByteString
+
 data Quote = Quote {
     packetTime  :: Clock.DiffTime,
     acceptTime  :: Clock.DiffTime,
@@ -39,6 +38,7 @@ data Quote = Quote {
     bids        :: [Bid],
     asks        :: [Bid]
   } deriving (Show)
+
 data Bid = Bid {
     price :: Int,
     quantity :: Int
@@ -100,53 +100,81 @@ safeRead s = case reads s of
 
 -- packet processing
 
-type Packet = BS.ByteString
-data PacketIter a = Finish a | GetPacket (Maybe Packet -> PacketIter a)
 
-instance Monad PacketIter where
+type Packet = BS.ByteString
+
+data PacketIter e a where
+  Finish :: a -> PacketIter e a
+  GetPacket :: (Maybe Packet -> PacketIter e a) -> PacketIter e a
+  OtherEffects :: e x -> (x -> PacketIter e a) -> PacketIter e a
+
+data Printing x where
+  Print :: String -> Printing ()
+
+data Exception x where
+  Throw :: String -> Exception x
+
+data Sum (f :: * -> *) (g :: * -> *) x = L (f x) | R (g x)
+
+printEff :: String -> PacketIter (Sum Printing e) ()
+printEff s = OtherEffects (L $ Print s) return
+
+instance Monad (PacketIter e) where
   return = Finish
   Finish a >>= f = f a
   GetPacket k >>= f = GetPacket (\a -> k a >>= f)
+  OtherEffects e k >>= f = OtherEffects e (\a -> k a >>= f)
 
-instance Applicative PacketIter where
+  -- OtherEffect replaces
+  -- Print s k >>= f = Print s (\a -> k a >>= f)
+  -- Throw s >>= f = Throw s
+
+instance Applicative (PacketIter e) where
   pure = return
   (<*>) = ap
 
-instance Functor PacketIter where
+instance Functor (PacketIter e) where
   fmap = liftM
 
-streamPackets :: FileName -> PacketIter a -> IO a
+streamPackets :: FileName -> PacketIter (Sum Printing Exception) a -> IO a
 streamPackets fname it = do
   handle <- Pcap.openOffline fname
   let process (Finish a) = return a
       process (GetPacket k) = do
           (hdr, bs) <- Pcap.toBS =<< Pcap.next handle
           process (k $ Just bs)
+      process (OtherEffects (L (Print s)) k) = do
+        putStrLn s
+        process (k ())
+      process (OtherEffects (R (Throw s)) _) = do
+        -- [todo] handle better
+        putStrLn s
+        error s
   process it
   -- [todo] close handle?
   -- [todo] handle EOF?
 
-getPacket :: PacketIter (Maybe Packet)
+getPacket :: PacketIter e (Maybe Packet)
 getPacket = GetPacket (Finish . maybe Nothing Just)
 
--- [hack] using unsafePerformIO, never returns
--- [todo] add IO effect to print as packets are processed
-getPackets :: PacketIter ()
+getPackets :: PacketIter _ ()
 getPackets = getPacket >>= \case
     Nothing -> getPackets
-    Just p  -> unsafePerformIO $ do
-          print $ uncurry parseQuotePacket $ parseUDPPacket p
-          return getPackets
+    Just p -> do
+        printEff $ show $ uncurry parseQuotePacket $ parseUDPPacket p
+        getPackets
 
-take' :: Int -> PacketIter a -> PacketIter a
+take' :: Int -> PacketIter e a -> PacketIter e a
 take' n (Finish a)    = Finish a
 take' 0 (GetPacket k) = GetPacket (\_ -> take' 0 (k Nothing))
-take' n (GetPacket k) = GetPacket (\p -> take' (n-1) (k p))
+take' n (GetPacket k) = GetPacket (take' (n-1) . k)
+take' n (OtherEffects e k) = OtherEffects e (take' n . k)
 
-filterPackets :: (Packet -> Bool) -> PacketIter a -> PacketIter a
+filterPackets :: (Packet -> Bool) -> PacketIter e a -> PacketIter e a
 filterPackets c (Finish a) = Finish a
 filterPackets c (GetPacket k) = GetPacket (filterPackets c . k .
       (>>= \p -> if c p then Just p else Nothing))
+filterPackets c (OtherEffects e k) = OtherEffects e (filterPackets c . k)
 
 -- file processing
 
