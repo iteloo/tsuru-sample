9c653ff8139aded1991efbb4829c04b0dd7d1cf0
Ting Chen Leo Hsu <iteloolab@gmail.com>
2016-10-03 15:44:38 +0900

    Lifts parsing to return `Maybe`, signifying parsing error - changes `Maybe a` to `Data a` when used to signify end of stream to avoid confusion - changes `filter` to only pass on values to the inner iteratee when data satisfying the condition is received

--- app/Main.hs
+++ app/Main.hs
@@ -14,20 +14,25 @@ import qualified Data.Time.LocalTime as LTime
 import qualified Data.Time.Clock as Clock
 import Control.Monad
 
+
 main :: IO ()
 main = do
-  let header = BS.take 5 . BS.drop 42
+  let header = range 42 5
   streamPackets "mdf-kospi200.20110216-0.pcap"
-    $ filter ((== BS.pack "B6034") . header . snd)
-    $ take 20
-    $ transform (liftM parsePacket)
-    $ transform (liftM $ uncurry parseQuotePacket)
+    $ filter (maybe False (== BS.pack "B6034") . header . snd)
+    $ take 20  -- [todo] remove on final version
+    $ transform (liftM quoteFromPacket)
     $ getForever
 
--- quote ac
+-- quote parsing
+
+type Packet = (Pcap.PktHdr, BS.ByteString)
+
+type Payload = BS.ByteString
 
 type QuotePacket = BS.ByteString
 
+-- [todo] statically verify bytestring lengths and formats using LiquidHaskell
 data Quote = Quote {
     packetTime  :: Clock.DiffTime,
     acceptTime  :: Clock.DiffTime,
@@ -42,77 +47,72 @@ data Bid = Bid {
   } deriving (Show)
 
 -- extracts substring of length `n` at location `i` of a bytestring
--- returns approximate result (but no exception) if out of bound
-range :: Int -> Int -> BS.ByteString -> BS.ByteString
-range i n = BS.take n . BS.drop i
+-- returns Nothing if out of bound
+range :: Int -> Int -> BS.ByteString -> Maybe BS.ByteString
+range i n bs =
+  let bs' = BS.take n . BS.drop i $ bs in
+  if n == BS.length bs' then Just bs' else Nothing
 
--- parses header and UDP packet into
-parsePacket :: Packet -> (QuotePacket, Clock.DiffTime)
-parsePacket (hdr, p) =
-  let s = fromIntegral $ Pcap.hdrSeconds hdr
-      ms = fromIntegral $ Pcap.hdrUseconds hdr
-      atime = Clock.picosecondsToDiffTime $ 10^12 * s + 10^6 * ms
-  in (range 42 215 p, atime)
+quoteFromPacket :: Packet -> Maybe Quote
+quoteFromPacket (hdr, pl) = do
+  qPkt <- quotePacketFromPayload pl
+  let atime = packetAcceptTimeFromHeader hdr
+  quoteFromQuotePacket qPkt atime
+
+-- parses header and UDP packet into quote packets
+quotePacketFromPayload :: Payload -> Maybe QuotePacket
+quotePacketFromPayload = range 42 215
+
+packetAcceptTimeFromHeader :: Pcap.PktHdr -> Clock.DiffTime
+packetAcceptTimeFromHeader hdr =
+  let s     = fromIntegral $ Pcap.hdrSeconds hdr
+      ms    = fromIntegral $ Pcap.hdrUseconds hdr
+  in Clock.picosecondsToDiffTime $ 10^12 * s + 10^6 * ms
 
 -- assumes packet begins with "B6034"
 -- and contains the right number of bytes
 -- packet time is passed in as an arg
--- [todo] statically verify bytestring lengths, etc, using LiquidHaskell
-parseQuotePacket :: QuotePacket -> Clock.DiffTime -> Quote
-parseQuotePacket p ptime =
-    let parseBids bs = fst $ foldr (\_ (bids, remain) ->
-                      let (bid, remain') = BS.splitAt 12 remain
-                          (p,q) = BS.splitAt 5 bid
-                      in (Bid
-                            (read $ BS.unpack p)
-                            (read $ BS.unpack q)
-                          : bids
-                          , remain')  -- [todo] convert to safeRead
-                    ) ([],bs) [1..5]
-  in Quote {
+quoteFromQuotePacket :: QuotePacket -> Clock.DiffTime -> Maybe Quote
+quoteFromQuotePacket p ptime = do
+  acceptTime <- parseAcceptTime =<< range 206 8 p
+  issueCode <- liftM BS.unpack $ range 5 12 p
+  bids <- parseBids =<< range 29 60 p
+  asks <- parseBids =<< range 96 60 p
+  return Quote {
     packetTime  = ptime,
-    acceptTime  = parsePacketTime $ range 206 8 p,
-    issueCode   = BS.unpack $ range 5 12 p,  -- [todo] safeRead
-    bids        = reverse $ parseBids $ range 29 60 p,
-    asks        = parseBids $ range 96 60 p
+    acceptTime  = acceptTime,
+    issueCode   = issueCode,
+    bids        = reverse bids,
+    asks        = asks
   }
+  where
+    -- assumes input is a bytestring of 8 digits
+    parseAcceptTime :: BS.ByteString -> Maybe Clock.DiffTime
+    parseAcceptTime bs = do
+      hh <- safeRead =<< liftM BS.unpack (range 0 2 bs)
+      mm <- safeRead =<< liftM BS.unpack (range 2 2 bs)
+      ss <- safeRead =<< liftM BS.unpack (range 4 2 bs)
+      uu <- safeRead =<< liftM BS.unpack (range 6 2 bs)
+      let pico = fromRational $ (fromIntegral ss) + (fromIntegral uu) / 100
+      return $ LTime.timeOfDayToTime $ LTime.TimeOfDay hh mm pico
 
--- assumes input is a bytestring of 8 digits
-parsePacketTime :: BS.ByteString -> Clock.DiffTime
-parsePacketTime bs =
-  let hh = read $ BS.unpack $ range 0 2 bs :: Int
-      mm = read $ BS.unpack $ range 2 2 bs :: Int
-      ss = read $ BS.unpack $ range 4 2 bs :: Int
-      uu = read $ BS.unpack $ range 6 2 bs :: Int
-      pico = fromRational $ (fromIntegral ss) + (fromIntegral uu) / 100
-  in LTime.timeOfDayToTime $ LTime.TimeOfDay hh mm pico
-
+    -- assumes input is a bytestring of 60 (= (5+7)*5) digits
+    -- [todo] check statically with LiquidHaskell
+    parseBids :: BS.ByteString -> Maybe [Bid]
+    parseBids bs =
+      liftM fst $ foldM (\(bids, remain) _ -> do
+            let (bid, remain') = BS.splitAt 12 remain
+            let (pstr, qstr) = BS.splitAt 5 bid
+            p <- safeRead $ BS.unpack pstr
+            q <- safeRead $ BS.unpack qstr
+            return (Bid p q : bids, remain')
+        ) ([],bs) [1..5]
 
 -- iteratees
-
-type Packet = (Pcap.PktHdr, BS.ByteString)
 
 data Iter e a where
   Finish :: a -> Iter e a
   Effect :: e x -> (x -> Iter e a) -> Iter e a
-
-data Get i x where
-  Get :: Get i i
-
-type GetPacket = Get (Maybe Packet)
-
-data Printing x where
-  Print :: String -> Printing ()
-
-data Exception x where
-  Throw :: String -> Exception x
-
--- [todo] implement using open unions instead
-data Sum (f :: * -> *) (g :: * -> *) (h :: * -> *) x
-    = G (f x) | P (g x) | T (h x)
-
-printEff :: String -> Iter (Sum _ Printing _) ()
-printEff s = Effect (P $ Print s) return
 
 instance Monad (Iter e) where
   return = Finish
@@ -126,17 +126,47 @@ instance Applicative (Iter e) where
 instance Functor (Iter e) where
   fmap = liftM
 
+data Get i x where
+  Get :: Get i i
+
+data Printing x where
+  Print :: String -> Printing ()
+
+data Exception x where
+  Throw :: String -> Exception x
+
+-- [todo] implement using open unions instead
+data Sum3 (f :: * -> *) (g :: * -> *) (h :: * -> *) x
+    = G (f x) | P (g x) | T (h x)
+
+printEff :: String -> Iter (Sum3 x Printing z) ()
+printEff s = Effect (P $ Print s) return
+
+data Data a = NoData | Data a
+
+instance Monad Data where
+  return = Data
+  NoData >>= f = NoData
+  Data a >>= f = f a
+
+instance Applicative Data where
+  pure = return
+  (<*>) = ap
+
+instance Functor Data where
+  fmap = liftM
+
 type FileName = String
 
 streamPackets :: FileName
-                  -> Iter (Sum GetPacket Printing Exception) a
+                  -> Iter (Sum3 (Get (Data Packet)) Printing Exception) a
                   -> IO a
 streamPackets fname it = do
   handle <- Pcap.openOffline fname
   let process (Finish a) = return a
       process (Effect (G Get) k) = do
           p <- Pcap.toBS =<< Pcap.next handle
-          process (k $ Just p)
+          process (k $ Data p)
       process (Effect (P (Print s)) k) = do
         putStrLn s
         process (k ())
@@ -148,32 +178,35 @@ streamPackets fname it = do
   -- [todo] close handle?
   -- [todo] handle EOF?
 
-get :: Iter _ (Maybe a)
+get :: Iter _ (Data a)
 get = Effect (G Get) Finish
 
 getForever :: Iter _ ()
-getForever = get >>= \case
-    Nothing -> getForever
-    Just p -> do
-        printEff $ show p
-        getForever
+getForever = do
+  a <- get
+  let msg = case a of
+        NoData -> "End of stream"
+        Data x -> show x
+  printEff msg
+  getForever
 
-take :: Int -> Iter (Sum (Get (Maybe i)) x y) a
-            -> Iter (Sum (Get (Maybe i)) x y) a
+take :: Int -> Iter (Sum3 (Get (Data i)) x y) a
+            -> Iter (Sum3 (Get (Data i)) x y) a
 take n (Finish a)    = Finish a
-take 0 (Effect (G Get) k) = Effect (G Get) (\_ -> take 0 (k Nothing))
+take 0 (Effect (G Get) k) = Effect (G Get) (\_ -> take 0 (k NoData))
 take n (Effect (G Get) k) = Effect (G Get) (take (n-1) . k)
 take n (Effect e k) = Effect e (take n . k)
 
-filter :: (i -> Bool) -> Iter (Sum (Get (Maybe i)) x y) a
-                      -> Iter (Sum (Get (Maybe i)) x y) a
+filter :: (i -> Bool) -> Iter (Sum3 (Get (Data i)) x y) a
+                      -> Iter (Sum3 (Get (Data i)) x y) a
 filter c (Finish a) = Finish a
-filter c (Effect (G Get) k) = Effect (G Get) (filter c . k .
-      (>>= \p -> if c p then Just p else Nothing))
+filter c e@(Effect (G Get) k) = Effect (G Get) (filter c . loop)
+    where loop NoData  = k NoData
+          loop (Data i) = if c i then k (Data i) else e
 filter c (Effect e k) = Effect e (filter c . k)
 
-transform :: (a -> b) -> Iter (Sum (Get b) x y) c
-                      -> Iter (Sum (Get a) x y) c
+transform :: (a -> b) -> Iter (Sum3 (Get b) x y) c
+                      -> Iter (Sum3 (Get a) x y) c
 transform f (Finish a)          = Finish a
 transform f (Effect (G Get) k)  = Effect (G Get) (transform f . k . f)
 transform f (Effect (P e) k)    = Effect (P e) (transform f . k)
