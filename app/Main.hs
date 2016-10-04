8726a52b15306206532897b38973f7e9482ebb0b
Ting Chen Leo Hsu <iteloolab@gmail.com>
2016-10-04 17:57:21 +0900

    Adds quote output formatting - using `Show` instance for now to keep `getForever` polymorphic, but may be a bad idea - implements flushing of buffer when `reorderQuotes` receives `NoData`

--- app/Main.hs
+++ app/Main.hs
@@ -30,62 +30,6 @@ main = do
       $ getForever
 
 
--- reorders quotes based on quote accept time
--- assumes that `pt - qt <= 3` for each quote,
-  -- where `pt` : packet accept time
-  --       `qt` : quote accept time
--- we implement this using a set buffer, ordered by `qt`
---   and a variable `pt_max`
--- when a new quote arrives, we
-  -- add it to the buffer,
-  -- update `pt_max` to using its `pt`
--- when a quote is requested, we
-  -- take the min `m` from the buffer
-    -- i.e. the quote with least `qt`
-  -- if no such `m` exists,
-    -- we send a request
-  -- if `pt_max - qt_m > 3`, we answer to the request using `m`
-    -- proof that `qt_m` <= qt_q` for any `q` in the buffer, and
-    -- for all `q`s that we might receive in the future:
-      -- suppose `q` is a quote in the buffer, then
-        -- `qt_m <= qt_q` by defn as the min
-      -- suppose `f` is a future quote, then since `pt_f > pt_max`,
-        -- `qt_m < pt_max - 3 < pt_f - 3 <= qt_f
-    -- proof that we cannot do better: if pt_max - qt_m = 3 - e, then
-    -- there could be a future `q` such that `qt_m > qt_q`
-      -- let `q` be a packet with
-        -- `qt_q = pt_max - 3 + e/2` and `pt_q = pt_max + e/3`
-      -- `qt_m = pt_max - 3 + e > qt_q`
-      -- `pt_q > pt_max`
-      -- `pt_q - qt_q = 3 - e/6 < 3`
-  -- otherwise, we send a request
-reorderQuotes :: Iter (Sum3 (Get (Data Quote)) x y) a
-              -> Iter (Sum3 (Get (Data Quote)) x y) a
-reorderQuotes = reord (T.posixSecondsToUTCTime 0) Set.empty  -- bogus initial pmax
-  where
-    reord :: T.UTCTime -> Set.Set Quote
-            -> Iter (Sum3 (Get (Data Quote)) x y) a
-            -> Iter (Sum3 (Get (Data Quote)) x y) a
-    reord pmax buf (Finish a) = Finish a
-    reord pmax buf (Effect (G Get) k) =
-      case Set.minView buf of
-        Just (q, buf') ->
-          if T.diffUTCTime pmax (acceptTime q) > maxOffset
-            then reord pmax buf' (k (Data q))
-            else request
-        Nothing -> request
-      where
-        request = Effect (G Get) $ \case
-          -- [todo] [fix] handle this case
-          NoData -> undefined
-          Data q ->
-            let pmax' = packetTime q
-                -- [todo] [fix] multiple quotes for same accept time
-                buf' = Set.insert q buf
-            in reord pmax' buf' (Effect (G Get) k)
-    reord pmax buf (Effect e k) = Effect e (reord pmax buf . k)
-
-
 -- quote parsing
 -- [todo] statically verify bytestring lengths and formats using LiquidHaskell
 
@@ -102,13 +46,29 @@ data Quote = Quote {
     issueCode   :: String,
     bids        :: [Bid],
     asks        :: [Bid]
-  } deriving (Show, Eq, Ord)
+  } deriving (Eq, Ord)
+
+instance Show Quote where
+  show = showQuote
 
 data Bid = Bid {
     price :: Int,
     quantity :: Int
   } deriving (Show, Eq, Ord)
 
+showQuote :: Quote -> String
+showQuote q = string $ fmap ($ q) [
+    show . packetTime,
+    show . acceptTime,
+    issueCode,
+    string . fmap showBid . bids,
+    string . fmap showBid . asks
+  ]
+  where
+    string = foldr (\a b -> a ++ " " ++ b) ""
+    showBid b = (show $ quantity b) ++ "@" ++ (show $ price b)
+
+maxOffset :: T.NominalDiffTime
 maxOffset = 3
 
 -- extracts substring of length `n` at location `i` of a bytestring
@@ -177,17 +137,17 @@ quoteFromQuotePacket p ptime = do
           atimes = do
             tz <- tzones
             let day = T.localDay $ T.utcToLocalTime tz ptime
-            -- [note] we have to account for the possibility that
+            -- here we account for the possibility that
             --   the quote was accepted on the previous local day,
             --   but the packet is accepted on the next day
-            -- [note] uniqueness relies on `maxOffset` to be less
+            -- [note] uniqueness relies on `maxOffset` being less
             --   than half a day
             d <- [day, T.addDays (-1) day]
             let t = T.LocalTime d aToD
             return $ T.localTimeToUTC tz t
       in
         -- [note] we expect exactly one result assuming that the
-        --   3s constraint is satisfied
+        --   `maxOffset` constraint is satisfied
         L.find ((< maxOffset) . T.diffUTCTime ptime) atimes
 
     -- assumes input is a bytestring of 60 (= (5+7)*5) digits
@@ -278,11 +238,13 @@ get = Effect (G Get) Finish
 getForever :: Iter _ ()
 getForever = do
   a <- get
-  let msg = case a of
-        NoData -> "No Data"
-        Data x -> show x
-  printEff msg
-  getForever
+  case a of
+    NoData -> do
+      printEff "End of stream"
+      return ()
+    Data x -> do
+      printEff $ show x
+      getForever
 
 take :: Int -> Iter (Sum3 (Get (Data i)) x y) a
             -> Iter (Sum3 (Get (Data i)) x y) a
@@ -320,6 +282,73 @@ transform f (Effect (T e) k)    = Effect (T e) (transform f . k)
 transformData :: (a -> b) -> Iter (Sum3 (Get (Data b)) x y) c
                       -> Iter (Sum3 (Get (Data a)) x y) c
 transformData f = transform $ liftM f
+
+-- reorders quotes based on quote accept time
+-- assumes that `pt - qt <= 3` for each quote,
+  -- where `pt` : packet accept time
+  --       `qt` : quote accept time
+-- we implement this using a set buffer, ordered by `qt`
+--   and a variable `pt_max`
+-- when a new quote arrives, we
+  -- add it to the buffer,
+  -- update `pt_max` to using its `pt`
+-- when a quote is requested, we
+  -- take the min `m` from the buffer
+    -- i.e. the quote with least `qt`
+  -- if no such `m` exists,
+    -- we send a request
+  -- if `pt_max - qt_m > 3`, we answer to the request using `m`
+    -- proof that `qt_m` <= qt_q` for any `q` in the buffer, and
+    -- for all `q`s that we might receive in the future:
+      -- suppose `q` is a quote in the buffer, then
+        -- `qt_m <= qt_q` by defn as the min
+      -- suppose `f` is a future quote, then since `pt_f > pt_max`,
+        -- `qt_m < pt_max - 3 < pt_f - 3 <= qt_f
+    -- proof that we cannot do better: if pt_max - qt_m = 3 - e, then
+    -- there could be a future `q` such that `qt_m > qt_q`
+      -- let `q` be a packet with
+        -- `qt_q = pt_max - 3 + e/2` and `pt_q = pt_max + e/3`
+      -- `qt_m = pt_max - 3 + e > qt_q`
+      -- `pt_q > pt_max`
+      -- `pt_q - qt_q = 3 - e/6 < 3`
+  -- otherwise, we send a request
+-- when EOF, flush everything in the buffer
+reorderQuotes :: Iter (Sum3 (Get (Data Quote)) x y) a
+              -> Iter (Sum3 (Get (Data Quote)) x y) a
+reorderQuotes = reord (T.posixSecondsToUTCTime 0) Set.empty  -- bogus initial pmax
+  where
+    reord :: T.UTCTime -> Set.Set Quote
+            -> Iter (Sum3 (Get (Data Quote)) x y) a
+            -> Iter (Sum3 (Get (Data Quote)) x y) a
+    reord pmax buf (Finish a) = Finish a
+    reord pmax buf (Effect (G Get) k) =
+      case Set.minView buf of
+        Just (q, buf') ->
+          if T.diffUTCTime pmax (acceptTime q) > maxOffset
+            then reord pmax buf' (k (Data q))
+            else request
+        Nothing -> request
+      where
+        request = Effect (G Get) $ \case
+          -- [todo] [fix] handle this case
+          NoData -> flush buf (Effect (G Get) k)
+          Data q ->
+            let pmax' = packetTime q
+                -- [todo] [fix] multiple quotes for same accept time
+                buf' = Set.insert q buf
+            in reord pmax' buf' (Effect (G Get) k)
+
+        flush :: Set.Set Quote
+                -> Iter (Sum3 (Get (Data Quote)) x y) a
+                -> Iter (Sum3 (Get (Data Quote)) x y) a
+        flush buf (Finish a)          = Finish a
+        flush buf (Effect (G Get) k)  =
+          case Set.minView buf of
+            Just (q, buf') -> flush buf' (k (Data q))
+            Nothing -> flush buf (k NoData)
+        flush buf (Effect e k)        = Effect e (flush buf . k)
+    reord pmax buf (Effect e k) = Effect e (reord pmax buf . k)
+
 
 -- helpers
 
