a5cea2051792537b3304dd90a4503f42048aff2e
Ting Chen Leo Hsu <iteloolab@gmail.com>
2016-10-09 17:11:23 +0800

    Abstracts handling of `Data a` - defines Kleisli composition `(>>>)` and flipped function composition `(.>)`

--- src/Iter.hs
+++ src/Iter.hs
@@ -20,7 +20,7 @@ data Iter e a where
 instance Monad (Iter e) where
   return = Finish
   Finish a >>= f = f a
-  Effect e k >>= f = Effect e (\a -> k a >>= f)
+  Effect e k >>= f = Effect e ((>>= f) . k)
 
 instance Applicative (Iter e) where
   pure = return
@@ -42,15 +42,13 @@ data Exception x where
 data Sum3 (f :: * -> *) (g :: * -> *) (h :: * -> *) x
     = G (f x) | P (g x) | T (h x)
 
-printS :: String -> Iter (Sum3 x Printing z) ()
-printS s = Effect (P $ Print s) return
-
 data Data a = NoData | Data a
 
 type Packet = (Pcap.PktHdr, BS.ByteString)
 
 type FileName = String
 
+-- enumerator for streaming contents of pcap files
 streamPackets :: FileName
                   -> Iter (Sum3 (Get (Data Packet)) Printing Exception) a
                   -> IO a
@@ -72,6 +70,9 @@ streamPackets fname it = do
   process it
   -- [note] no need/way to close handle
 
+
+-- get effect
+
 -- get :: Iter (Sum3 (Get a) g h) a
 get = Effect (G Get) return
 
@@ -79,7 +80,6 @@ getForever :: Show i => Iter (Sum3 (Get (Data i)) Printing z) ()
 getForever = get >>= \case
     NoData -> do
       printS "End of stream"
-      return ()
     Data x -> do
       printS $ show x
       getForever
@@ -104,49 +104,39 @@ handleGetS f s (Effect (T e) k) = Effect (T e) (handleGetS f s . k)
 handleGet f = handleGetS g ()
   where g h _ k = f (h ()) k
 
+-- handleData :: Monad m => (t -> m (Data a)) -> Data t -> m (Data a)
+handleData f = \case
+  NoData -> return NoData
+  Data i -> f i
+
 -- drop :: Int -> Iter (Sum3 (Get (Data i)) x y) a
 --               -> Iter (Sum3 (Get (Data i)) x y) a
 drop = handleGetS f
   where f h 0 k = get >>= k
-        f h n k = get >>= h (n-1) . \case
-                    NoData -> k NoData
-                    Data _ -> get >>= k
+        f h n k = get >>= handleData (const get) >>> k .> h (n-1)
 
 -- take :: Int -> Iter (Sum3 (Get (Data i)) x y) a
 --             -> Iter (Sum3 (Get (Data i)) x y) a
 take = handleGetS f
   where f h 0 k = h 0 (k NoData)
-        f h n k = get >>= h (n-1) . k
+        f h n k = get >>= k .> h (n-1)
 
 -- filter :: (i -> Bool) -> Iter (Sum3 (Get (Data i)) x y) a
 --                       -> Iter (Sum3 (Get (Data i)) x y) a
 filter c = handleGet f
-  where f h k = get >>= h . \case
-          NoData -> k NoData
-          Data i -> if c i then k (Data i) else get >>= k
+  where f h k = get >>= handleData (\i -> if c i then return (Data i) else get)
+                    >>> k .> h
 
 -- a filter that blocks `Nothing` and output `a` for `Just a`
 -- filterMaybe :: Iter (Sum3 (Get (Data i)) x y) a
 --               -> Iter (Sum3 (Get (Data (Maybe i))) x y) a
 filterMaybe = handleGet f
-  where f h k = get >>= h . \case
-                  NoData        -> k NoData
-                  Data (Just i) -> k $ Data i
-                  _             -> get >>= k
+  where f h k = get >>= handleData (maybe get (return . Data)) >>> k .> h
 
 -- transform :: (a -> b) -> Iter (Sum3 (Get b) x y) c
 --                       -> Iter (Sum3 (Get a) x y) c
 transform f = handleGet g
-  where g h k = get >>= h . k . \case
-          NoData -> NoData
-          Data i -> Data (f i)
-
--- [think] is Set really the right choice? no duplicate would be stored
-
--- a stateful handler that receives data and store them into an ordered
---   buffer. It keeps a state, which is updated through a supplied
---   update function when new data is received. When data is requested,
---   it
+  where g h k = get >>= handleData (return . Data . f) >>> k .> h
 
 -- a stateful handler that stores data into a buffer as they
 --   are received. The user supplies a conditional that can depend on
@@ -157,6 +147,7 @@ transform f = handleGet g
 --      (i -> i -> Bool)
 --      -> Iter (Sum3 (Get (Data i)) x y) a
 --      -> Iter (Sum3 (Get (Data i)) x y) a
+-- [think] is Set really the right choice? no duplicate would be stored
 reorder cond = handleGetS f (undefined, Set.empty)
   -- [note] no branch where `undefined` is evaluated
   where
@@ -178,3 +169,22 @@ reorder cond = handleGetS f (undefined, Set.empty)
           case Set.minView buf of
             Just (i, buf') -> h buf' (k (Data i))
             Nothing -> h buf (k NoData)
+
+
+-- print effect
+
+printS :: String -> Iter (Sum3 x Printing z) ()
+printS s = Effect (P $ Print s) return
+
+
+-- helpers
+
+-- Kleisli composition
+(>>>) :: Monad m => (a -> m b) -> (b -> m c) -> a -> m c
+(f >>> g) a = f a >>= g
+infixl 9 >>>
+
+-- reverse function composition
+(.>) :: (a -> b) -> (b -> c) -> a -> c
+(.>) = flip (.)
+infixl 9 .>
